fa490d375756073de71dcc53516ed458658e11f6
Shyamnandhan R <68549505+Shyamnandhan@users.noreply.github.com>
2023-05-08 23:28:23 +0530

    Create test_integrate.py

---
+++ test_integrate.py
@@ -0,0 +1,18 @@
+# unit test case
+import unittest
+
+class animal:
+x = 'Lion'
+
+class TestMethods(unittest.TestCase):
+	# test function to test object equality of two value
+	def test_positive(self):
+		firstValue = animal()
+		secondValue = 'Lion'
+		# error message in case if test case got failed
+		message = "First value and second value are not evaluated to same object !"
+		# assertIs() to check that if first & second evaluated to same object
+		self.assertIs(firstValue, secondValue, message)
+
+if __name__ == '__main__':
+	unittest.main()
