1f5d838a8b0569fb2d1929bb1faee00ec26579d6
Shyamnandhan R <68549505+Shyamnandhan@users.noreply.github.com>
2023-05-09 11:25:09 +0530

    Test case for bird

--- test_integrate.py
+++ test_integrate.py
@@ -5,7 +5,7 @@ class TestStringMethods(unittest.TestCase):
 	# test function to test equality of two value
 	def test_negative(self):
 		container = "Bear Cow Elephant Bird Sheep"
-		animal = "Wolf"
+		animal = "Bird"
 		# error message in case if test case got failed
 		message = animal + " is not in the trained model"
 		# assertEqual() to check equality of first & second value
